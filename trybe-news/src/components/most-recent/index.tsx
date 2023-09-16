5479f24b49dc335d52c02d7a71674aec8af706e7
Frederico Brion <fredericoasbrion@gmail.com>
2023-09-16 10:07:43 -0300

    feat: estilizando noticia principal

--- trybe-news/src/components/most-recent/index.tsx
+++ trybe-news/src/components/most-recent/index.tsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import NewsContext from "../../context/NewsContext";
 import { Images, News } from "../../types";
 import daysPosted from "../../services/daysPosted";
+import styles from './most-recent.module.css';
+
+import emptyHeart from '../../assets/images/empty-heart.svg';
+import filledHeart from '../../assets/images/filled-heart.svg';
 
 const BASE_URL = 'https://agenciadenoticias.ibge.gov.br/';
 
@@ -16,14 +20,22 @@ function MostRecent() {
   const postedDate = fetchCompleted ? news[0].data_publicacao : '';
 
   return (
-    <div>
-      <img src={ `${BASE_URL}${images.image_intro}` } alt="Imagem na noticia principal" />
-      <p>Noticia mais recente</p>
-      <h2>{ mostRecentNew.titulo }</h2>
-      <p>{ mostRecentNew.introducao }</p>
-      <p>{ daysPosted(postedDate) }</p>
-      <Link to='/'>Leia a noticia</Link>
-      <button>Favoritar</button>
+    <div className={ styles.container }>
+      <img
+        className={ styles.mainImage }
+        src={ `${BASE_URL}${images.image_intro}` }
+        alt="Imagem na noticia principal"
+      />
+      <div>
+        <h6>Notícia mais recente</h6>
+        <button>
+          <img src={ filledHeart } alt="" />
+        </button>
+        <h2>{ mostRecentNew.titulo }</h2>
+        <p>{ mostRecentNew.introducao }</p>
+        <span>{ daysPosted(postedDate) }</span>
+        <Link to='/' className={ styles.link }>Leia a noticia</Link>
+      </div>
     </div>
   );
 }
