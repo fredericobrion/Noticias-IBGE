6c3c7af0f1cbf3a89be1efffbf47c1fab61bb45a
Frederico Brion <fredericoasbrion@gmail.com>
2023-09-19 18:22:48 -0300

    test: preparando ambiente

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,7 +3,7 @@ import App from '../App';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import NewsProvider from '../context/NewsProvider';
-import { render } from 'react-dom';
+import { render } from '@testing-library/react';
 
 describe('Verifica o funcionamento da página', () => {
   beforeEach(() => {
